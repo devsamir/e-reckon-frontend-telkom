5139218f2c6a74f7a801cd84dfbec92aa5c7deb0
devsamir <56377354+devsamir@users.noreply.github.com>
2022-11-02 02:08:33 +0700

    add login functionality

---
+++ src/components/form/FInputPassword.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Input, InputProps } from 'antd';
+import {
+  ExclamationCircleFilled,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from '@ant-design/icons';
+import { useFormContext, Controller } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+
+interface Props extends InputProps {
+  name: string;
+  defaultValue?: string;
+}
+
+const FInput: React.FC<Props> = ({ name, defaultValue = '', ...props }) => {
+  const {
+    formState: { errors },
+
+    control,
+  } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      defaultValue={defaultValue}
+      control={control}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <React.Fragment>
+          <Input.Password
+            {...props}
+            onChange={onChange}
+            value={value}
+            status={error?.message && 'error'}
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+          />
+          <ErrorMessage
+            errors={errors}
+            name={name}
+            render={({ message }) => (
+              <div className="text-red-500 flex items-center gap-2">
+                <ExclamationCircleFilled />
+                <small className="text-sm">{message}</small>
+              </div>
+            )}
+          />
+        </React.Fragment>
+      )}
+    />
+  );
+};
+
+export default FInput;
---
+++ src/services/login.service.ts
@@ -0,0 +1,14 @@
+import { useMutation } from '@tanstack/react-query';
+import ApiCall from './ApiCall';
+
+interface LoginData {
+  username: string;
+  password: string;
+}
+
+export const useLoginService = () => {
+  const loginMutation = useMutation({
+    mutationFn: (data: LoginData) => ApiCall.post('/auth/login', data),
+  });
+  return { loginMutation };
+};
