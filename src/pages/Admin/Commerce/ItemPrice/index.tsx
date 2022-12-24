0852ee50ec46351e978f389fadd7d009eac08197
devsamir <56377354+devsamir@users.noreply.github.com>
2022-12-24 18:57:57 +0700

    add update price functionality in commerce

--- src/pages/Admin/Commerce/ItemPrice/index.tsx
+++ src/pages/Admin/Commerce/ItemPrice/index.tsx
@@ -3,11 +3,12 @@ import { useForm, FormProvider } from "react-hook-form";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useQueryClient } from "@tanstack/react-query";
-import { Breadcrumb, Button, Modal, notification } from "antd";
+import { Breadcrumb, Modal, notification } from "antd";
 import Pagination, { usePagination } from "src/components/Pagination";
 import TableExtended from "src/components/TableExtended";
 import UnitSelector from "src/components/filters/UnitSelector";
 import FInput from "src/components/form/FInput";
+import FInputNumber from "src/components/form/FInputNumber";
 import { pick, removeFalsyValue } from "src/helpers/utils";
 import { useItemService } from "src/services/item.service";
 
@@ -21,21 +22,11 @@ const Item = () => {
     useItemService(pagination);
   const [id, setId] = useState();
   const [showModal, setShowModal] = useState(false);
-  const [confirmDelete, setConfirmDelete] = useState(false);
 
   const form = useForm({ resolver: yupResolver(itemSchema) });
 
   // Function for Crud Preparation
-  const prepareCreate = () => {
-    setShowModal(true);
-    setId(null);
-    form.reset({
-      item_code: "",
-      material_designator: "",
-      service_designator: "",
-      unit_id: "",
-    });
-  };
+
   const prepareEdit = (record) => {
     setShowModal(true);
     setId(record?.id);
@@ -45,14 +36,15 @@ const Item = () => {
         "material_designator",
         "service_designator",
         "unit_id",
+        "material_price_telkom",
+        "service_price_telkom",
+        "material_price_mitra",
+        "service_price_mitra",
       ])
     );
   };
-  const prepareDelete = (record) => {
-    setConfirmDelete(true);
-    setId(record?.id);
-  };
-  const columns = useItemColumns(prepareEdit, prepareDelete);
+
+  const columns = useItemColumns(prepareEdit);
 
   // Function for Crud Action
   const onSubmit = async (values) => {
@@ -89,27 +81,12 @@ const Item = () => {
       });
     }
   };
-  const handleDelete = async () => {
-    await deleteMutation.mutateAsync([id], {
-      onSuccess: () => {
-        queryClient.invalidateQueries(["getAllItem"]);
-        notification.success({ message: "Berhasil hapus item" });
-      },
-      onError: (error: any) => {
-        notification.error({
-          message: error?.response?.data?.message || error?.message,
-        });
-      },
-      onSettled: () => {
-        setConfirmDelete(false);
-      },
-    });
-  };
+
   return (
     <>
       <Breadcrumb>
         <Breadcrumb.Item>Admin</Breadcrumb.Item>
-        <Breadcrumb.Item>Warehouse</Breadcrumb.Item>
+        <Breadcrumb.Item>Commerce</Breadcrumb.Item>
         <Breadcrumb.Item>Item</Breadcrumb.Item>
       </Breadcrumb>
       <FilterItem setDomain={pagination.setDomain} />
@@ -120,9 +97,6 @@ const Item = () => {
           limit={pagination.limit}
           onChange={pagination.onChangePagination}
         />
-        <Button type="primary" onClick={prepareCreate}>
-          Tambah
-        </Button>
       </div>
       <TableExtended
         columns={columns}
@@ -139,8 +113,8 @@ const Item = () => {
       />
       <FormProvider {...form}>
         <Modal
-          title={!!id ? "Edit Item" : "Tambah Item"}
-          okText={!!id ? "Update" : "Create"}
+          title={"Edit Item Price"}
+          okText={"Update Item Price"}
           open={showModal}
           onOk={form.handleSubmit(onSubmit)}
           confirmLoading={createMutation.isLoading || updateMutation.isLoading}
@@ -148,35 +122,50 @@ const Item = () => {
         >
           <div className="flex flex-col gap-1 mb-4">
             <label className="text-sm font-medium">Kode Item</label>
-            <FInput name="item_code" placeholder="Input" />
+            <FInput name="item_code" placeholder="Input" disabled />
           </div>
           <div className="flex flex-col gap-1 mb-4">
             <label className="text-sm font-medium">Material Designator</label>
-            <FInput name="material_designator" placeholder="Input" />
+            <FInput name="material_designator" placeholder="Input" disabled />
           </div>
           <div className="flex flex-col gap-1 mb-4">
             <label className="text-sm font-medium">Service Designator</label>
-            <FInput name="service_designator" placeholder="Input" />
+            <FInput name="service_designator" placeholder="Input" disabled />
           </div>
           <div className="flex flex-col gap-1 mb-4">
             <label className="text-sm font-medium">Unit</label>
-            <UnitSelector name="unit_id" placeholder="Select" />
+            <UnitSelector name="unit_id" placeholder="Select" disabled />
+          </div>
+          <div className="flex flex-col gap-1 mb-4">
+            <label className="text-sm font-medium">Harga Material Telkom</label>
+            <FInputNumber
+              name="material_price_telkom"
+              placeholder="Input Number"
+            />
+          </div>
+          <div className="flex flex-col gap-1 mb-4">
+            <label className="text-sm font-medium">Harga Service Telkom</label>
+            <FInputNumber
+              name="service_price_telkom"
+              placeholder="Input Number"
+            />
+          </div>
+          <div className="flex flex-col gap-1 mb-4">
+            <label className="text-sm font-medium">Harga Material Mitra</label>
+            <FInputNumber
+              name="material_price_mitra"
+              placeholder="Input Number"
+            />
+          </div>
+          <div className="flex flex-col gap-1 mb-4">
+            <label className="text-sm font-medium">Harga Service Mitra</label>
+            <FInputNumber
+              name="service_price_mitra"
+              placeholder="Input Number"
+            />
           </div>
         </Modal>
       </FormProvider>
-      <Modal
-        title={"Delete Item"}
-        okText={"Delete"}
-        open={confirmDelete}
-        okButtonProps={{
-          style: { background: "rgb(239 68 68)", border: "none" },
-        }}
-        onOk={handleDelete}
-        confirmLoading={deleteMutation.isLoading}
-        onCancel={setConfirmDelete.bind(this, false)}
-      >
-        Yakin hapus data item ?
-      </Modal>
     </>
   );
 };
